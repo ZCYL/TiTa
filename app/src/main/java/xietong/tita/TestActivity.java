1584acb2c6106aff4a3774a7f72e4e1ffe6d7559
TeenTeam <Dairowu@163.com>
2015-07-24 14:00:43 +0800

    Test

--- app/src/main/java/xietong/tita/TestActivity.java
+++ app/src/main/java/xietong/tita/TestActivity.java
@@ -11,6 +11,7 @@ public class TestActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_test);
     }
 }
